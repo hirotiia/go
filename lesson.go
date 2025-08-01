1312921ab868e4e96aecba1c80f82f2c9c81bcdb
Hiroya Nakano <hiroya_0610_15mro@me.com>
2025-08-01 20:13:20 +0900

    :zap: go定義

--- lesson.go
+++ lesson.go
@@ -2,23 +2,29 @@ package main
 
 import (
 	"fmt"
-	"os/user"
-	"time"
 )
 
-func init() {
-	fmt.Println(time.Now().Format(time.RFC3339))
-	i := 42
-	o := 0.172832
-	fmt.Printf("%T\n", i)
-	fmt.Printf("%T\n", o)
+// Q1. 以下の1.11をint型に変換して出力してください。
+func lesson1() int {
+	f := 1.11
+	i := int(f)
+	return i
 }
 
-func bazz() {
-	fmt.Println(user.Current())
+// Q2. コードを書かずに以下の出力結果を答えてください。
+func lesson2() []int {
+	s := []int{1, 2, 5, 6, 2, 3, 1}
+	return s[2:4]
 }
 
+// Q3. 以下のコードを実行した時に
+func lesson3() map[string]int {
+	m := map[string]int{"Mike": 20, "Nancy": 25, "Messi": 30}
+	return m
+}
 func main() {
-	bazz()
-	fmt.Println("Hello, World!")
+	fmt.Println(lesson1())
+	fmt.Println(lesson2())
+	m := lesson3()
+	fmt.Printf("%T %v\n", m, m)
 }
